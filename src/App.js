3cdcad8526d33b894289273a2d5a46a2db7e76b8
guylan <guylan.noel@laposte.net>
2022-05-27 12:01:26 +0200

    changement d'emplacement de la db pour l'import, changement de la fonction de Games + ajout de la fonction InfoGames

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import History from './components/history/History';
 import Users from './components/users/Users';
 import Contact from './components/contact/Contact';
 import Games from './components/games/Games';
+import InfoGame from './components/infogame/InfoGame';
+import axios from 'axios';
 
 export default function App() {
-
+  
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -17,6 +19,7 @@ export default function App() {
       <Route path="histoire" element={<History />} />
       <Route path="membres" element={<Users />} />
       <Route path="contact" element={<Contact />} />
+      <Route path="jeux/:name" element={<InfoGame />} />
     </Routes>
   );
 }
