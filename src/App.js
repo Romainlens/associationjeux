366a00189737d0ea3c1fac3b0d39464e0779378b
guylan <guylan.noel@laposte.net>
2022-05-31 14:08:59 +0200

    ajout de la fonction de contact

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Users from './components/users/Users';
 import Contact from './components/contact/Contact';
 import Games from './components/games/Games';
 import InfoGame from './components/infogame/InfoGame';
-import axios from 'axios';
 
 export default function App() {
   
